8b2f701b5522737f55e46849c74e5ee157fa3f30
Blanca Ubis <ubis.blanca@gmail.com>
2022-12-20 11:32:32 +0100

    Add new endpoint to get movies, add filter by gender, add sort by title

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const movies = require('./data/movies.json');
 
 // create and config server
 const server = express();
@@ -10,4 +11,40 @@ server.use(express.json());
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
+});
+
+server.get('/movies', (req, res) => {
+  const genderFilterParam = req.query.gender;
+  const sortFilterParam = req.query.sort;
+  const filteredMovies = movies.filter((eachMovie) => {
+    return eachMovie.gender.includes(genderFilterParam);
+  });
+  if (sortFilterParam === 'asc') {
+    filteredMovies.sort((a, b) => {
+      if (a.title < b.title) {
+        return -1;
+      } else if (a.title > b.title) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  } else {
+    filteredMovies.sort((a, b) => {
+      if (a.title < b.title) {
+        return -1;
+      } else if (a.title > b.title) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    filteredMovies.reverse();
+  }
+
+  const response = {
+    success: true,
+    movies: filteredMovies,
+  };
+  res.json(response);
 });
--- web/src/services/api-movies.js
+++ web/src/services/api-movies.js
@@ -1,36 +1,22 @@
 // login
 
-const getMoviesFromApi = () => {
+const getMoviesFromApi = (params) => {
   console.log('Se están pidiendo las películas de la app');
-  // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-  return fetch('//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/empty.json')
-    .then(response => response.json())
-    .then(() => {
-      // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
-      return {
-        success: true,
-        movies: [
-          {
-            id: '1',
-            title: 'Gambita de dama',
-            gender: 'Drama',
-            image:
-              '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/gambito-de-dama.jpg'
-          },
-          {
-            id: '2',
-            title: 'Friends',
-            gender: 'Comedia',
-            image:
-              '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/friends.jpg'
-          }
-        ]
-      };
+  console.log(params);
+  return fetch(
+    `//localhost:4000/movies?gender=${params.gender}&sort=${params.sort}`,
+    {
+      method: 'GET',
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
     });
 };
 
 const objToExport = {
-  getMoviesFromApi: getMoviesFromApi
+  getMoviesFromApi: getMoviesFromApi,
 };
 
 export default objToExport;
